cf432676f791db4c2671bc7e4d390bf0253b0466
gsonkow <sonkowskyg@gmail.com>
2025-04-03 04:33:36 -0700

    statistics functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const CARDS_OPENED = 5
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [deckSize, setDeckSize] = useState(40)
+  // PLACEHOLDER FOR HARD CODED CARD
+  const [tempCardQuantity, setTempCardQuantity] = useState(3)
 
   return (
     <>
       <h1>YGO Ratio Visualizer</h1>
-      <div className="card">
-        time to build my app
+      <div className="calculator">
+        Deck Size: <input type="number" value={deckSize} onChange={e => setDeckSize(e.target.value)} />
+        <br></br>
+        
+        <input type="text" placeHolder='Card Name' value='Ash Blossom & Joyous Spring' />
+        <input type="number" value={tempCardQuantity} onChange={e => setTempCardQuantity(e.target.value)} />
+      </div>
+      <div className='result'>
+        
       </div>
     </>
   )
+}
+
+function calculateProbabilities(deckSize, cardQuantity) {
+  
+}
+
+function factorial(n){
+  return n > 1 ? n * factorial(n - 1) : 1
+}
+
+function combination(m, n) {
+  return factorial(m) / (factorial(n) * factorial(m - n))
+}
+
+function hypergeometric(N, K, n, k) {
+  return (combination(K, k) * combination(N - K, n - k)) / combination(N, n)
 }
 
 export default App
