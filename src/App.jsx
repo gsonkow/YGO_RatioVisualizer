e4eea482014bbcba63557cee43fc42026aa70e30
gsonkow <sonkowskyg@gmail.com>
2025-05-05 01:54:05 -0700

    mobile layout+styling  improvements

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ const DEFAULT_TAGS = [
   'Discard Cost/Effect',
   'Bad Draw Going First',
   'Bad Draw Going Second',
-  'Hand Trap', 
-  'Board Breaker', 
+  'Hand Trap',
+  'Board Breaker',
   'Starter',
   'Extender',
 ]
@@ -58,7 +58,7 @@ function App() {
   const [ygoApiData, setYgoApiData] = useState(null)
 
   //Setup Example deck
-  const [setupCounter , setSetupCounter] = useState(0)
+  const [setupCounter, setSetupCounter] = useState(0)
 
 
   //TODO(?): validate tag input
@@ -73,7 +73,7 @@ function App() {
       value: `Engine ${i + 1}`,
       label: `Engine ${i + 1}`
     }));
-    const customTags = tags.filter(tag => 
+    const customTags = tags.filter(tag =>
       !DEFAULT_TAGS.some(defaultTag => defaultTag === tag.value) &&
       !engineTags.some(engineTag => engineTag.value === tag.value) &&
       !/^Engine \d+$/.test(tag.value)
@@ -81,7 +81,7 @@ function App() {
     setTags([...DEFAULT_TAGS.map(tag => ({ value: tag, label: tag })), ...engineTags, ...customTags]);
   }
 
-  
+
   function importYDKE(url) {
     var passcodes = []
     setImporting(true)
@@ -94,7 +94,7 @@ function App() {
       setImporting(false)
       return
     }
-    
+
     setImportCardsQuantity(Array.from(passcodes.reduce((map, passcode) => {
       map.set(passcode, (map.get(passcode) || 0) + 1)
       return map
@@ -104,7 +104,7 @@ function App() {
     })))
 
     const uniquePasscodes = [...new Set(passcodes)].join(', ')
-    
+
     setSetupCounter(setupCounter + 1)
 
     setImportMessage('Importing YDKE URL...')
@@ -124,13 +124,13 @@ function App() {
   useEffect(() => {
     if (ygoApiData && importCardsQuantity) {
       const newCards = ygoApiData.data.map(card => {
-      const importedCard = importCardsQuantity.find(imported => imported.passcode === card.id)
-      return {
-        id: card.id,
-        name: card.name,
-        quantity: importedCard ? importedCard.quantity : 1,
-        tags: []
-      }
+        const importedCard = importCardsQuantity.find(imported => imported.passcode === card.id)
+        return {
+          id: card.id,
+          name: card.name,
+          quantity: importedCard ? importedCard.quantity : 1,
+          tags: []
+        }
       })
       setCards(newCards)
       const numCards = newCards.reduce((sum, card) => sum + card.quantity, 0)
@@ -149,51 +149,57 @@ function App() {
   useEffect(() => {
     //console.log('Setting up example deck...')
     handleEngineChange(numEngines)
-    importYDKE(EXAMPLE_DECK) 
+    importYDKE(EXAMPLE_DECK)
   }, [])
 
 
   return (
     <>
-      <h1><img id='Logo' src={Logo} alt="YGO Ratio Visualizer"/></h1>
-      <h3>By <a id="git link" href="https://github.com/gsonkow/" target='_blank'><img id='gitLogo' src={GitLogo} alt="Github Logo"/>gsonkow</a> 
-      &emsp;<a href="https://ko-fi.com/gsonkow" target="_blank"><img id='Ko-fi' src={Kofi} alt="Support me on Ko-fi"/></a></h3>
+      <meta name="viewport" content="width=500"></meta>
+      <header>
+        <img id='Logo' src={Logo} alt="YGO Ratio Visualizer" />
+        <span>By <a id="git link" href="https://github.com/gsonkow/" target='_blank'><img id='gitLogo' src={GitLogo} alt="Github Logo" />gsonkow</a>
+        &emsp;<a href="https://ko-fi.com/gsonkow" target="_blank"><img id='Ko-fi' src={Kofi} alt="Support me on Ko-fi" /></a></span>
+      </header>
       <p id='helper'>-Scroll down to see results!-</p>
       <div id="calculator">
         <button disabled={importing} onClick={() => importYDKE(ydkeURL)}>{importMessage}</button>
-        &emsp;<input type="text" style={{width: '50%'}} placeholder='YDKE URL' value={ydkeURL} onChange={e => setYDKEURL(e.target.value)}/>
+        &emsp;<input type="text" style={{ width: '50%' }} placeholder='YDKE URL' value={ydkeURL} onChange={e => setYDKEURL(e.target.value)} />
         <br></br>
-        Deck Size: &nbsp;
-        <button id='decrement' onClick={() => {
-          if (deckSize > minDeckSize) {
-            setDeckSize(deckSize - 1)
-          }
-        }}>-</button>
-        <input type="number" min={minDeckSize} value={deckSize} style={{width: '50px'}} onChange={e => setDeckSize(e.target.value)} />
-        <button id='increment' onClick={() => { setDeckSize(deckSize + 1) }}>+</button>
-        &emsp;
-        
-        Number of Engines: &nbsp;
-        <button id='decrement' onClick={() => {
-          if (numEngines > 1) {
-            handleEngineChange(numEngines - 1)
-          }
-        }}>-</button><input type="number" min="1" value={numEngines} style={{width: '50px'}} onChange={e => {
-          handleEngineChange(Number(e.target.value))
+        <span id="deckSetting">
+          Deck Size: &nbsp;
+          <button id='decrement' onClick={() => {
+            if (deckSize > minDeckSize) {
+              setDeckSize(deckSize - 1)
+            }
+          }}>-</button>
+          <input id="deckSetting" type="number" min={minDeckSize} value={deckSize} style={{ width: '50px' }} onChange={e => setDeckSize(e.target.value)} />
+          <button id='increment' onClick={() => { setDeckSize(deckSize + 1) }}>+</button>
+          &emsp;
+
+          Number of Engines: &nbsp;
+          <button id='decrement' onClick={() => {
+            if (numEngines > 1) {
+              handleEngineChange(numEngines - 1)
+            }
+          }}>-</button>
+          <input id="deckSetting" type="number" min="1" value={numEngines} style={{ width: '50px' }} onChange={e => {
+            handleEngineChange(Number(e.target.value))
           }} />
-        <button id='increment' onClick={() => { handleEngineChange(numEngines + 1) }}>+</button>
-        &emsp;
-        <button onClick={() => {
-          setCards([])
-          setDeckSize(40)
-          handleEngineChange(1)
-        }}> Clear Deck </button>
+          <button id='increment' onClick={() => { handleEngineChange(numEngines + 1) }}>+</button>
+          &emsp;
+          <button onClick={() => {
+            setCards([])
+            setDeckSize(40)
+            handleEngineChange(1)
+          }}> Clear Deck </button>
+        </span>
 
         {/* Card input and editor */}
         {cards.map((card, index) => {
           return (
             <div id="card" key={index}>
-              <button  id="remove" onClick={() => {
+              <button id="remove" onClick={() => {
                 const newCards = [...cards]
                 newCards.splice(index, 1)
                 setCards(newCards)
@@ -201,7 +207,7 @@ function App() {
                 setMinDeckSize(totalQuantity)
                 if (deckSize < totalQuantity) { setDeckSize(totalQuantity) }
               }}>X</button>
-              <input id='name'type="text" placeholder='Card Name' value={card.name} onChange={e => {
+              <input id='name' type="text" placeholder='Card Name' value={card.name} onChange={e => {
                 const newCards = [...cards]
                 newCards[index].name = e.target.value
                 setCards(newCards)
@@ -232,8 +238,8 @@ function App() {
                 setMinDeckSize(totalQuantity)
                 if (deckSize < totalQuantity) { setDeckSize(totalQuantity) }
               }}>+</button>
-              <CreatableSelect 
-                value={card.tags} 
+              <CreatableSelect
+                value={card.tags}
                 id='tags'
                 styles={{
                   control: (base) => ({
@@ -283,9 +289,9 @@ function App() {
                   }),
                 }}
                 isClearable
-                isMulti 
-                options={tags} 
-                closeMenuOnSelect={false} 
+                isMulti
+                options={tags}
+                closeMenuOnSelect={false}
                 components={makeAnimated()}
                 onCreateOption={handleCreateTag}
                 onChange={newValue => {
@@ -298,34 +304,37 @@ function App() {
             </div>
           )
         })}
-        
+
         <br></br>
-        Deck Size: &nbsp;
-        <button id='decrement' onClick={() => {
-          if (deckSize > minDeckSize) {
-            setDeckSize(deckSize - 1)
-          }
-        }}>-</button>
-        <input type="number" min={minDeckSize} value={deckSize} style={{width: '50px'}} onChange={e => setDeckSize(e.target.value)} />
-        <button id='increment' onClick={() => { setDeckSize(deckSize + 1) }}>+</button>
-        &emsp;
-        
-        Number of Engines: &nbsp;
-        <button id='decrement' onClick={() => {
-          if (numEngines > 1) {
-            handleEngineChange(numEngines - 1)
-          }
-        }}>-</button><input type="number" min="1" value={numEngines} style={{width: '50px'}} onChange={e => {
-          handleEngineChange(Number(e.target.value))
+        <span id="deckSetting">
+          Deck Size: &nbsp;
+          <button id='decrement' onClick={() => {
+            if (deckSize > minDeckSize) {
+              setDeckSize(deckSize - 1)
+            }
+          }}>-</button>
+          <input id="deckSetting" type="number" min={minDeckSize} value={deckSize} style={{ width: '50px' }} onChange={e => setDeckSize(e.target.value)} />
+          <button id='increment' onClick={() => { setDeckSize(deckSize + 1) }}>+</button>
+          &emsp;
+
+          Number of Engines: &nbsp;
+          <button id='decrement' onClick={() => {
+            if (numEngines > 1) {
+              handleEngineChange(numEngines - 1)
+            }
+          }}>-</button>
+          <input id="deckSetting" type="number" min="1" value={numEngines} style={{ width: '50px' }} onChange={e => {
+            handleEngineChange(Number(e.target.value))
           }} />
-        <button id='increment' onClick={() => { handleEngineChange(numEngines + 1) }}>+</button>
-        &emsp;
-        <button onClick={() => {
-          setCards([...cards, {id: cards.length + 1, name: '', quantity: 1, tags: []}])
-          const totalQuantity = [...cards, {id: cards.length + 1, name: '', quantity: 1}].reduce((sum, card) => sum + parseInt(card.quantity || 0, 10), 0)
-          setMinDeckSize(totalQuantity)
-          if (deckSize < totalQuantity) { setDeckSize(totalQuantity) }
+          <button id='increment' onClick={() => { handleEngineChange(numEngines + 1) }}>+</button>
+          &emsp;
+          <button onClick={() => {
+            setCards([...cards, { id: cards.length + 1, name: '', quantity: 1, tags: [] }])
+            const totalQuantity = [...cards, { id: cards.length + 1, name: '', quantity: 1 }].reduce((sum, card) => sum + parseInt(card.quantity || 0, 10), 0)
+            setMinDeckSize(totalQuantity)
+            if (deckSize < totalQuantity) { setDeckSize(totalQuantity) }
           }}>Add Card</button>
+        </span>
 
       </div>
       <br></br>
@@ -333,7 +342,7 @@ function App() {
         {Calculator(cards, tags, deckSize, numEngines)}
       </div>
       <footer>
-        <a href="https://github.com/gsonkow/YGO_RatioVisualizer" target='_blank'><img id='gitLogo' src={GitLogo} alt="Github Logo"/>Project</a> built with <a href="https://www.npmjs.com/package/ydke" target='_blank'>ydke.js</a> and <a href='https://ygoprodeck.com/api-guide/' target='_blank'>Yu-Gi-Oh! API by YGOPRODeck</a> 
+        <a href="https://github.com/gsonkow/YGO_RatioVisualizer" target='_blank'><img id='gitLogo' src={GitLogo} alt="Github Logo" />Project</a> built with <a href="https://www.npmjs.com/package/ydke" target='_blank'>ydke.js</a> and <a href='https://ygoprodeck.com/api-guide/' target='_blank'>Yu-Gi-Oh! API by YGOPRODeck</a>
       </footer>
     </>
   )
